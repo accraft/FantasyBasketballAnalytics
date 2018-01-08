c842b79bb7c5542a366c119bfde75517a8b4ad97
AC Craft <accraft4@gmail.com>
2018-01-08 02:03:39 -0500

    Got the function that pulls data using chrome cookies working! Function returns a string though and scare_scores expects a file path

--- ParseScores.py
+++ ParseScores.py
@@ -1,32 +1,31 @@
 import os
 
 #Point of this program is to scrape webpage that displays fantasy bball results
-def scrapeScores(HTML_Page):
+def scrape_scores(HTML_Page):
     from bs4 import BeautifulSoup
-    LineString=""
-    FantasyBBallPage = HTML_Page
+    line_string=""
 
-    CC_Data = BeautifulSoup(open(FantasyBBallPage),'lxml')
-    Header_TR = CC_Data.find("tr",class_="tableSubHead")
-    All_TR = CC_Data.find_all("tr",class_="linescoreTeamRow")
+    CC_Data = BeautifulSoup(open(HTML_Page),'lxml')
+    header_TR = CC_Data.find("tr",class_="tableSubHead")
+    all_TR = CC_Data.find_all("tr",class_="linescoreTeamRow")
 
     #first add headers
-    Header_TH = Header_TR.find_all("th")
-    for TD in Header_TH:
-        LineString = LineString + "," + TD.text
-    CombinedResults = LineString
-    LineString=""
+    header_TR = header_TR.find_all("th")
+    for TD in header_TR:
+        LineString = line_string + "," + TD.text
+    combined_results = line_string
+    line_string=""
 
     #now populate the scores for the given week
-    for TR in All_TR:
-        All_TD = TR.find_all("td")
-        for TD in All_TD:
-            LineString = LineString + "," + TD.text
-        CombinedResults = CombinedResults + "\n" + LineString.replace(",,",",")
-        LineString=""
+    for TR in all_TR:
+        all_TD = TR.find_all("td")
+        for TD in all_TD:
+            line_string = line_string + "," + TD.text
+        combined_results = combined_results + "\n" + line_string.replace(",,",",")
+        line_string=""
 
 
-    return CombinedResults;
+    return combined_results
 
 #sample call below 
 def main():
@@ -35,39 +34,56 @@ def main():
 
     #sample call below
     full_test_file_path = os.path.join(script_directory, 'JaVale Vindicated Scoreboard_ Matchup 11 (Dec 25 - 31) - ESPN.html')
-    print scrapeScores(full_test_file_path)
+    print scrape_scores(full_test_file_path)
 
 if __name__ == '__main__':
     main()
 
 
-#Below code is not yet wrapped in a function, right now it just attempts to print the title 
-#also requires the browsercookie package and a reasonable up to date version of sqlite3
-#now define a function that pulls the hmtl content associated with a generic URL
-import urllib2
+#Below code requires the browsercookie package and a reasonably up to date version of sqlite3
+#create new function, will take 2 arguments: URL of the site (required), UseChromeCookies (optional, TRUE if you want to use cookies from your chrome browser)
+#will return a string
+def pull_page(URL,use_chrome_cookies=False):
+    #type: (str, bool) -> str
+    import urllib2
+    from bs4 import BeautifulSoup
+    
+    if use_chrome_cookies==True:
+        import browsercookie
+        br = browsercookie.chrome()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(br))
+        page = opener.open(URL)
+    else:
+        page = urllib2.urlopen(URL)
+    return page.read()
+
+#sample call
+#PullPage('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',UseChromeCookies=True)
+
+#below is a nice little utility to print all lines (within the top 40 lines) between the title tags of a BeautifulSoup object
+def ReturnTitle(BS4_Object):
+    i = 0
+    p = -1
+    for line in BS4_Object.prettify().split('\n'):
+        i = i + 1
+        if i <= 40:
+            if 'title>' in line.lower():
+                p = p * -1
+            if p == 1:
+                print i
+                print line
+        else: break
+
 from bs4 import BeautifulSoup
-import browsercookie
+ReturnTitle(BeautifulSoup(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',use_chrome_cookies=True),'lxml'))
+    #JaVale Vindicated Scoreboard: Matchup 12 (Jan 1 - 7) -  ESPN
+ReturnTitle(BeautifulSoup(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',use_chrome_cookies=False),'lxml'))
+    # Log In -  ESPN
 
-#uncomment the below if you want to see the results of the page without including chrome cookies
-#page = urllib2.urlopen('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12')
 
-#below lines read in the URL with cookies
-br = browsercookie.chrome()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(br))
-page = opener.open('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12')
+#not sure how I"m going to tie this together, the below code does NOT work because pull_page returns a string and scare_scores expects a file path.
+scrape_scores(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=1',use_chrome_cookies=True))
+#maybe adjust scrape_scores to accept a string?
 
-#loops through and only displayes lines between the Title HTML tags
-page_bs = BeautifulSoup(page.read(),'lxml')
-i = 0
-p = -1
-for line in page_bs.prettify().split('\n'):
-    i = i + 1
-    if i <= 40:
-        if 'title>' in line.lower():
-            p = p * -1
-        if p == 1:
-            print i
-            print line
-    else: break
 
 
