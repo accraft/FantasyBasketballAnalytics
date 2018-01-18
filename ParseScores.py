4d5d356c6a971b80cffe040e09372cd6a8ac130f
AC Craft <accraft4@gmail.com>
2018-01-18 00:36:35 -0500

    ParseScores program yields a useable .csv file

--- ParseScores.py
+++ ParseScores.py
@@ -1,8 +1,9 @@
 import os
+import sys
+from bs4 import BeautifulSoup
 
 #Point of this program is to scrape webpage that displays fantasy bball results
 def scrape_scores(HTML_Page):
-    from bs4 import BeautifulSoup
     line_string=""
 
     CC_Data = BeautifulSoup(open(HTML_Page),'lxml')
@@ -12,33 +13,26 @@ def scrape_scores(HTML_Page):
     #first add headers
     header_TR = header_TR.find_all("th")
     for TD in header_TR:
-        LineString = line_string + "," + TD.text
-    combined_results = line_string
+        line_string = line_string + "," + TD.text
+    combined_results = line_string + "\n"
     line_string=""
 
     #now populate the scores for the given week
     for TR in all_TR:
         all_TD = TR.find_all("td")
+        line_string = CC_Data.find_all('h1')[1].text #adds the title of the matchup in the first column
         for TD in all_TD:
             line_string = line_string + "," + TD.text
-        combined_results = combined_results + "\n" + line_string.replace(",,",",")
+        combined_results = combined_results + line_string.replace(",,",",")  + "\n"
         line_string=""
 
 
+
     return combined_results
+#samplecall
+#scrape_scores("C:\\Users\\accra_000\\PycharmProjects\\AnalyseBBallLeague\\JaVale Vindicated Scoreboard_ Matchup 11 (Dec 25 - 31) - ESPN.html")
 
-#sample call below 
-def main():
-    path_to_script = os.path.realpath(__file__)
-    script_directory = os.path.dirname(path_to_script)
-
-    #sample call below
-    full_test_file_path = os.path.join(script_directory, 'JaVale Vindicated Scoreboard_ Matchup 11 (Dec 25 - 31) - ESPN.html')
-    print scrape_scores(full_test_file_path)
-
-if __name__ == '__main__':
-    main()
-
+#CC_Data = BeautifulSoup(open("C:\\Users\\accra_000\\PycharmProjects\\AnalyseBBallLeague\\JaVale Vindicated Scoreboard_ Matchup 11 (Dec 25 - 31) - ESPN.html"), 'lxml')
 
 #Below code requires the browsercookie package and a reasonably up to date version of sqlite3
 #create new function, will take 2 arguments: URL of the site (required), UseChromeCookies (optional, TRUE if you want to use cookies from your chrome browser)
@@ -60,30 +54,37 @@ def pull_page(URL,use_chrome_cookies=False):
 #sample call
 #PullPage('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',UseChromeCookies=True)
 
-#below is a nice little utility to print all lines (within the top 40 lines) between the title tags of a BeautifulSoup object
-def ReturnTitle(BS4_Object):
-    i = 0
-    p = -1
-    for line in BS4_Object.prettify().split('\n'):
-        i = i + 1
-        if i <= 40:
-            if 'title>' in line.lower():
-                p = p * -1
-            if p == 1:
-                print i
-                print line
-        else: break
+#below code ties it all together,
+def main(weeks,final_csv):
+    a=1
+    while a <= weeks:
+        html_file_contents = pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId='+str(a),use_chrome_cookies=True)
+        html_file_name = 'matchup' + str(a) + '.html'
+        text_file = open('matchup' + str(a) + '.html', "w+")
+        text_file.write(html_file_contents)
+        text_file.close()
 
-from bs4 import BeautifulSoup
-ReturnTitle(BeautifulSoup(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',use_chrome_cookies=True),'lxml'))
-    #JaVale Vindicated Scoreboard: Matchup 12 (Jan 1 - 7) -  ESPN
-ReturnTitle(BeautifulSoup(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=12',use_chrome_cookies=False),'lxml'))
-    # Log In -  ESPN
+        final_file = open(final_csv,'a+')
+        final_file.write(scrape_scores(html_file_name)+ "\n")
+        final_file.close()
+        print "Week " , a , " parsed."
+        a = a + 1
 
-
-#not sure how I"m going to tie this together, the below code does NOT work because pull_page returns a string and scare_scores expects a file path.
-scrape_scores(pull_page('http://games.espn.com/fba/scoreboard?leagueId=73636&matchupPeriodId=1',use_chrome_cookies=True))
-#maybe adjust scrape_scores to accept a string?
+#main pulls all 16 weeks
+if __name__ == '__main__':
+    main(16, 'C:\\Users\\accra_000\\PycharmProjects\\AnalyseBBallLeague\\all_scores.csv')
 
 
 
+
+
+
+
+#sample call below
+# def main():
+#     path_to_script = os.path.realpath(__file__)
+#     script_directory = os.path.dirname(path_to_script)
+#
+#     #sample html file already downloaded
+#     full_test_file_path = os.path.join(script_directory, 'JaVale Vindicated Scoreboard_ Matchup 11 (Dec 25 - 31) - ESPN.html')
+#     print scrape_scores(full_test_file_path)
